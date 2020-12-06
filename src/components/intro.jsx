1921bc6655632091408ca2c78eba092313c6fb82
rajanvalencia <rajan.valencia@au.com>
2020-12-06 18:49:26 +0900

    minor changes

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./stars.scss";
 import Typed from "react-typed";
 import logo1 from "../img/rajanvalencia.JPG";
@@ -9,12 +9,127 @@ export default function Intro() {
 
   const [isMouseOnEnter, setIsMouseOnEnter] = useState(false);
 
+  const [start, setStart] = useState({
+    MultiYouthCEO_English: true,
+    MultiYouthCEO_Japanese: true,
+    FullstackWebDeveloper_English: false,
+    FullstackWebDeveloper_Japanese: false,
+    TheMulticulturalYouthProjectDeputyRepesentative_English: false,
+    TheMulticulturalYouthProjectDeputyRepesentative_Japanese: false
+  });
+
+  const [complete, setComplete] = useState({
+    MultiYouthCEO_English: false,
+    MultiYouthCEO_Japanese: false,
+    FullstackWebDeveloper_English: false,
+    FullstackWebDeveloper_Japanese: false,
+    TheMulticulturalYouthProjectDeputyRepesentative_English: false,
+    TheMulticulturalYouthProjectDeputyRepesentative_Japanese: false
+  });
+
+  useEffect(() => {
+    async function handle() {
+      await setTimeout(handleOnComplete, 1000);
+    }
+    handle();
+  }, [complete]);
+
+  const handleOnComplete = () => {
+    if (complete.MultiYouthCEO_English && complete.MultiYouthCEO_Japanese) {
+      setStart({
+        ...start,
+        MultiYouthCEO_English: false,
+        MultiYouthCEO_Japanese: false,
+        FullstackWebDeveloper_English: true,
+        FullstackWebDeveloper_Japanese: true
+      });
+      setComplete({
+        ...complete,
+        MultiYouthCEO_English: false,
+        MultiYouthCEO_Japanese: false
+      });
+    }
+
+    if (complete.FullstackWebDeveloper_English && complete.FullstackWebDeveloper_Japanese) {
+      setStart({
+        ...start,
+        FullstackWebDeveloper_English: false,
+        FullstackWebDeveloper_Japanese: false,
+        TheMulticulturalYouthProjectDeputyRepesentative_English: true,
+        TheMulticulturalYouthProjectDeputyRepesentative_Japanese: true
+      });
+      setComplete({
+        ...complete,
+        FullstackWebDeveloper_English: false,
+        FullstackWebDeveloper_Japanese: false
+      });
+    }
+
+    if (complete.TheMulticulturalYouthProjectDeputyRepesentative_English && complete.TheMulticulturalYouthProjectDeputyRepesentative_Japanese) {
+      setStart({
+        ...start,
+        TheMulticulturalYouthProjectDeputyRepesentative_English: false,
+        TheMulticulturalYouthProjectDeputyRepesentative_Japanese: false,
+        MultiYouthCEO_English: true,
+        MultiYouthCEO_Japanese: true
+      });
+      setComplete({
+        ...complete,
+        TheMulticulturalYouthProjectDeputyRepesentative_English: false,
+        TheMulticulturalYouthProjectDeputyRepesentative_Japanese: false
+      })
+    }
+  }
+
+
   const onMouseEnter = () => {
     setIsMouseOnEnter(true);
   }
 
   const onMouseLeave = () => {
     setIsMouseOnEnter(false);
+  }
+
+  const onMultiYouthCEO_EnglishComplete = () => {
+    setComplete({
+      ...complete,
+      MultiYouthCEO_English: true
+    });
+  }
+
+  const onMultiYouthCEO_JapaneseComplete = () => {
+    setComplete({
+      ...complete,
+      MultiYouthCEO_Japanese: true
+    });
+  }
+
+  const onFullstackWebDeveloper_EnglishComplete = () => {
+    setComplete({
+      ...complete,
+      FullstackWebDeveloper_English: true
+    });
+  }
+
+  const onFullstackWebDeveloper_JapaneseComplete = () => {
+    setComplete({
+      ...complete,
+      FullstackWebDeveloper_Japanese: true
+    });
+  }
+
+  const onTheMulticulturalYouthProjectDeputyRepesentative_EnglishComplete = () => {
+    setComplete({
+      ...complete,
+      TheMulticulturalYouthProjectDeputyRepesentative_English: true
+    });
+  }
+
+  const onTheMulticulturalYouthProjectDeputyRepesentative_JapaneseComplete = () => {
+    setComplete({
+      ...complete,
+      TheMulticulturalYouthProjectDeputyRepesentative_Japanese: true
+    });
   }
 
   return (
@@ -36,31 +151,87 @@ export default function Intro() {
             <p className="intro-subtitle">
               <span className="text-slider-items"></span>
               <strong className="text-slider">
-                <Typed
-                  strings={[
-                    "MultiYouth CEO",
-                    "Full-stack Web Developer",
-                    "The Multicultural Youth Project Deputy Representative"
-                  ]}
-                  style={{ fontSize: '20px' }}
-                  typeSpeed={30}
-                  backDelay={1100}
-                  backSpeed={30}
-                  loop
-                />
-                <br />
-                <Typed
-                  strings={[
-                    "マルチユース 最高経営責任者",
-                    "フルスタックエンジニア",
-                    "多文化ユースプロジェクト副代表"
-                  ]}
-                  style={{ fontSize: '20px' }}
-                  typeSpeed={90}
-                  backDelay={1100}
-                  backSpeed={30}
-                  loop
-                />
+                {start.MultiYouthCEO_English && start.MultiYouthCEO_Japanese ?
+                  <Fragment>
+                    <Typed
+                      strings={[
+                        "MultiYouth Founder ・ CEO"
+                      ]}
+                      style={{ fontSize: '20px' }}
+                      typeSpeed={50}
+                      backDelay={1100}
+                      backSpeed={90}
+                      onComplete={onMultiYouthCEO_EnglishComplete}
+                    />
+                    <br />
+                    <Typed
+                      strings={[
+                        "MultiYouth 創立者 ・ 最高経営責任者",
+                      ]}
+                      style={{ fontSize: '20px' }}
+                      typeSpeed={50}
+                      backDelay={1100}
+                      backSpeed={90}
+                      onComplete={onMultiYouthCEO_JapaneseComplete}
+                    />
+                  </Fragment>
+                  : null
+                }
+                {start.FullstackWebDeveloper_English && start.FullstackWebDeveloper_Japanese ?
+                  <Fragment>
+                    <Typed
+                      strings={[
+                        "Full Stack Web Developer",
+                      ]}
+                      style={{ fontSize: '20px' }}
+                      typeSpeed={40}
+                      backDelay={1100}
+                      backSpeed={90}
+                      onComplete={onFullstackWebDeveloper_EnglishComplete}
+                    />
+                    <br />
+                    <Typed
+                      strings={[
+                        "フルスタックエンジニア",
+                      ]}
+                      style={{ fontSize: '20px' }}
+                      typeSpeed={90}
+                      backDelay={1100}
+                      backSpeed={90}
+                      onComplete={onFullstackWebDeveloper_JapaneseComplete}
+                    />
+                  </Fragment>
+                  :
+                  null
+                }
+                {start.TheMulticulturalYouthProjectDeputyRepesentative_English && start.TheMulticulturalYouthProjectDeputyRepesentative_Japanese ?
+                  <Fragment>
+                    <Typed
+                      strings={[
+                        "The Multicultural Youth Project Deputy Representative"
+                      ]}
+                      style={{ fontSize: '20px' }}
+                      typeSpeed={20}
+                      backDelay={1100}
+                      backSpeed={90}
+                      start={start.TheMulticulturalYouthProjectDeputyRepesentative_English}
+                      onComplete={onTheMulticulturalYouthProjectDeputyRepesentative_EnglishComplete}
+                    />
+                    <br />
+                    <Typed
+                      strings={[
+                        "多文化ユースプロジェクト副代表"
+                      ]}
+                      style={{ fontSize: '20px' }}
+                      typeSpeed={90}
+                      backDelay={1100}
+                      backSpeed={90}
+                      onComplete={onTheMulticulturalYouthProjectDeputyRepesentative_JapaneseComplete}
+                    />
+                  </Fragment>
+                  :
+                  null
+                }
               </strong>
             </p>
             <div style={{ maxWidth: '400px', display: 'block', marginRight: 'auto', marginLeft: 'auto' }}>
