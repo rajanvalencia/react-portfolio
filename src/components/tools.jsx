5a36a1c992840e10391ac250fe2d1b0aaa1ff9f8
rajanvalencia <rajan.valencia@au.com>
2021-10-01 23:24:26 +0900

    minor changes

--- src/components/tools.jsx
+++ src/components/tools.jsx
@@ -7,27 +7,27 @@ export default function Tools() {
             {
                 link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
-                alt: 'javascript'
+                alt: 'Javascript'
             },
             {
                 link: 'https://www.java.com',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg',
-                alt: 'java'
+                alt: 'Java'
             },
             {
                 link: 'https://www.php.net',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg',
-                alt: 'php'
+                alt: 'PHP'
             },
             {
                 link: 'https://python.org/',
                 img: 'https://cdn.worldvectorlogo.com/logos/python-5.svg',
-                alt: 'python'
+                alt: 'Python'
             },
             {
                 link: 'https://www.w3schools.com/cpp/',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg',
-                alt: 'c++'
+                alt: 'C++'
             },
             {
                 link: 'https://jquery.com/',
@@ -37,228 +37,233 @@ export default function Tools() {
             {
                 link: 'https://www.w3schools.com/css/',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg',
-                alt: 'css3'
+                alt: 'CSS3'
             }, 
             {
                 link: 'https://www.w3.org/html/',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
-                alt: 'html5'
+                alt: 'HTML5'
             }, 
         ],
         frameworks : [
             {
                 link: 'https://reactjs.org/',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
-                alt: 'react'
+                alt: 'React'
             },
             {
                 link: 'https://nextjs.org/',
                 img: 'https://cdn.worldvectorlogo.com/logos/nextjs-2.svg',
-                alt: 'nextjs'
+                alt: 'NextJs'
             }, 
             {
                 link: 'https://nodejs.org/',
                 img: 'https://cdn.worldvectorlogo.com/logos/nodejs-1.svg',
-                alt: 'nodejs'
+                alt: 'NodeJs'
             },
             {
                 link: 'https://spring.io/',
                 img: 'https://www.vectorlogo.zone/logos/springio/springio-icon.svg',
-                alt: 'spring'
+                alt: 'Spring'
             }, 
             {
                 link: 'https://spring.io/projects/spring-boot',
                 img: 'https://cdn.worldvectorlogo.com/logos/spring-14.svg',
-                alt: 'spring boot'
+                alt: 'Spring Boot'
             }, 
         ],
         database:[
             {
                 link: 'https://www.mysql.com/',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg',
-                alt: 'mysql'
+                alt: 'MySQL'
             }, 
             {
                 link: 'https://mariadb.org/',
                 img: 'https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg',
-                alt: 'mariadb'
+                alt: 'MariaDB'
             }, 
         ],
         aws: [
             {
                 link: 'https://aws.amazon.com/amplify/',
                 img: 'https://docs.amplify.aws/assets/logo-dark.svg',
-                alt: 'amplify'
+                alt: 'Amplify'
             },
             {
                 link: 'https://aws.amazon.com/certificate-manager/',
                 img: 'https://symbols.getvecta.com/stencil_23/3_aws-certificate-manager.b79c71d4c3.svg',
-                alt: 'certificate manager'
+                alt: 'Certificate Manager'
             },
             {
                 link: 'https://aws.amazon.com/cloudformation/',
                 img: 'https://symbols.getvecta.com/stencil_16/5_aws-cloudformation.89d93ec8e8.svg',
-                alt: 'cloud formation'
+                alt: 'Cloud Formation'
             },
             {
                 link: 'https://aws.amazon.com/cloudfront/',
                 img: 'https://symbols.getvecta.com/stencil_20/0_amazon-cloudfront.c349f9d1dc.svg',
-                alt: 'cloudfront'
+                alt: 'Cloudfront'
             },
             {
                 link: 'https://aws.amazon.com/cloudwatch/',
                 img: 'https://symbols.getvecta.com/stencil_73/84_amazon-cloudwatch-icon.10d39b18a6.svg',
-                alt: 'cloudwatch'
+                alt: 'Cloudwatch'
             },
             {
                 link: 'https://aws.amazon.com/codebuild/',
                 img: 'https://symbols.getvecta.com/stencil_12/0_aws-codebuild.0c1cad5388.svg',
-                alt: 'codebuild'
+                alt: 'Codebuild'
             },
             {
                 link: 'https://aws.amazon.com/codepipeline/',
                 img: 'https://symbols.getvecta.com/stencil_12/3_aws-codepipeline.2b22fe114b.svg',
-                alt: 'codepipeline'
+                alt: 'Code Pipeline'
             },
             {
                 link: 'https://aws.amazon.com/codestar/',
                 img: 'https://symbols.getvecta.com/stencil_12/5_aws-codestar.a7175af236.svg',
-                alt: 'codestar'
+                alt: 'Codestar'
             },
             {
                 link: 'https://aws.amazon.com/ec2/',
                 img: 'https://symbols.getvecta.com/stencil_9/0_ec2.607c2cfeab.svg',
-                alt: 'ec2'
+                alt: 'EC2'
             },
             {
                 link: 'https://aws.amazon.com/elasticbeanstalk/',
                 img: 'https://symbols.getvecta.com/stencil_9/32_aws-elastic-beanstalk.e06f11f2b0.svg',
-                alt: 'elastic beanstalk'
+                alt: 'Elastic Beanstalk'
             },
             {
                 link: 'https://aws.amazon.com/ecr/',
                 img: 'https://symbols.getvecta.com/stencil_9/12_amazon-ecr.e2bb590423.svg',
-                alt: 'elastic container registry'
+                alt: 'Elastic Container Registry'
             },
             {
                 link: 'https://aws.amazon.com/ecs/',
                 img: 'https://symbols.getvecta.com/stencil_9/14_amazon-ecs.0defeec970.svg',
-                alt: 'elastic container service'
+                alt: 'Elastic Container Service'
             },
             {
                 link: 'https://aws.amazon.com/iam/',
                 img: 'https://symbols.getvecta.com/stencil_23/11_iam.ff93ec62a2.svg',
-                alt: 'iam'
+                alt: 'IAM'
             },
             {
                 link: 'https://aws.amazon.com/lambda/',
                 img: 'https://symbols.getvecta.com/stencil_74/144_aws-lambda-icon.70ee4ffc8e.svg',
-                alt: 'lambda'
+                alt: 'Lambda'
             },
             {
                 link: 'https://aws.amazon.com/rds/',
                 img: 'https://symbols.getvecta.com/stencil_10/12_amazon-rds.b2cd85c3d2.svg',
-                alt: 'rds'
+                alt: 'RDS'
             },
             {
                 link: 'https://aws.amazon.com/route53/',
                 img: 'https://cdn.worldvectorlogo.com/logos/aws-route53.svg',
-                alt: 'route53'
+                alt: 'Route53'
             },
             {
                 link: 'https://aws.amazon.com/s3/',
                 img: 'https://symbols.getvecta.com/stencil_24/6_amazon-s3.88314aefee.svg',
-                alt: 's3'
+                alt: 'S3'
             },
             {
                 link: 'https://aws.amazon.com/simpleemailservice/',
                 img: 'https://symbols.getvecta.com/stencil_17/1_amazon-ses.f2cd79b845.svg',
-                alt: 'simple email service'
+                alt: 'Simple Email Service'
             },
         ],
         os:[
             {
                 link: 'https://aws.amazon.com/jp/amazon-linux-2/',
                 img: 'https://cdn.worldvectorlogo.com/logos/amazon-web-services-2.svg',
-                alt: 'amazon linux 2'
+                alt: 'Amazon Linux 2'
             }, 
             {
                 link: 'https://ubuntu.com/',
                 img: 'https://cdn.worldvectorlogo.com/logos/ubuntu-4.svg',
-                alt: 'ubuntu'
+                alt: 'Ubuntu'
             }, 
             {
                 link: 'https://www.centos.org/',
                 img: 'https://cdn.worldvectorlogo.com/logos/centos-1.svg',
-                alt: 'centos'
+                alt: 'CentOS'
             },
 
             {
                 link: 'https://www.linux.org/',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg',
-                alt: 'linux'
+                alt: 'Linux'
             },
             {
                 link: 'https://www.apple.com/macos/big-sur/',
                 img: 'https://cdn.worldvectorlogo.com/logos/macos.svg',
-                alt: 'macos'
+                alt: 'MacOS'
             }, 
         ],
         others : [
             {
                 link: 'https://git-scm.com/',
                 img: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg',
-                alt: 'git'
+                alt: 'Git'
             }, 
             {
                 link: 'https://github.com',
                 img: 'https://cdn.worldvectorlogo.com/logos/github-icon-1.svg',
-                alt: 'github'
+                alt: 'Github'
             },
             {
                 link: 'https://firebase.google.com/',
                 img: 'https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg',
-                alt: 'firebase'
+                alt: 'Firebase'
             },
             {
                 link: 'https://www.docker.com/',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg',
-                alt: 'docker'
+                alt: 'Docker'
             },
             {
                 link: 'https://oauth.net/2/',
                 img: 'https://cdn.worldvectorlogo.com/logos/oauth.svg',
-                alt: 'oauth2'
+                alt: 'OAuth2'
             },
             {
                 link: 'https://www.gnu.org/software/bash/',
                 img: 'https://www.vectorlogo.zone/logos/gnu_bash/gnu_bash-icon.svg',
-                alt: 'bash'
+                alt: 'Bash'
             }, 
             {
                 link: 'https://getbootstrap.com',
                 img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg',
-                alt: 'bootstrap'
-            },  
-            {
-                link: 'https://materializecss.com/',
-                img: 'https://cdn.worldvectorlogo.com/logos/materialize-css-1.svg',
-                alt: 'materializecss'
+                alt: 'Bootstrap'
             },  
             {
                 link: 'https://postman.com',
                 img: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg',
-                alt: 'postman'
+                alt: 'PostMan'
             }, 
             {
                 link: 'https://code.visualstudio.com/',
                 img: 'https://cdn.worldvectorlogo.com/logos/visual-studio-code-1.svg',
-                alt: 'visual studio code'
+                alt: 'VS Code'
             }, 
             {
                 link: 'https://stripe.com/',
                 img: 'https://cdn.worldvectorlogo.com/logos/stripe-4.svg',
-                alt: 'stripe'
+                alt: 'Stripe'
+            }, 
+            {
+                link: 'https://mapbox.com/',
+                img: 'https://cdn.worldvectorlogo.com/logos/mapbox-2.svg',
+                alt: 'Mapbox'
+            }, 
+            {
+                link: 'https://fontawesome.com/',
+                img: 'https://cdn.worldvectorlogo.com/logos/fontawesome-1.svg',
+                alt: 'FontAwesome'
             }, 
         ]
     };
